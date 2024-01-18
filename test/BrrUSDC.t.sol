7d8fff5b7131f62535c5fae5526a5fcad8c0f98d
kphed <kpcrypto69@gmail.com>
2024-01-18 10:08:04 -0500

    Add initialize tests

---
+++ test/BrrUSDC.t.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC20} from "solady/tokens/ERC20.sol";
+import {Initializable} from "solady/utils/Initializable.sol";
+import {Helper} from "test/Helper.sol";
+import {BrrUSDC} from "src/BrrUSDC.sol";
+
+contract BrrUSDCTest is Helper {
+    /*//////////////////////////////////////////////////////////////
+                             initialize
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotInitializeInvalidInitialization() external {
+        vm.expectRevert(Initializable.InvalidInitialization.selector);
+
+        vault.initialize(
+            COMET_REWARDS,
+            ROUTER,
+            INITIAL_REWARD_FEE,
+            admin,
+            admin
+        );
+    }
+
+    function testInitialize() external {
+        BrrUSDC uninitializedVault = BrrUSDC(
+            // Deploys a new proxy but does not initialize.
+            ERC1967_FACTORY.deploy(vaultImplementation, admin)
+        );
+
+        assertEq(address(0), address(uninitializedVault.cometRewards()));
+        assertEq(address(0), address(uninitializedVault.router()));
+        assertEq(0, uninitializedVault.rewardFee());
+        assertEq(address(0), uninitializedVault.protocolFeeReceiver());
+        assertEq(address(0), uninitializedVault.feeDistributor());
+
+        vm.expectEmit(true, true, true, true, address(uninitializedVault));
+
+        emit Initializable.Initialized(1);
+
+        uninitializedVault.initialize(
+            COMET_REWARDS,
+            ROUTER,
+            INITIAL_REWARD_FEE,
+            admin,
+            admin
+        );
+
+        assertEq(COMET_REWARDS, address(uninitializedVault.cometRewards()));
+        assertEq(ROUTER, address(uninitializedVault.router()));
+        assertEq(INITIAL_REWARD_FEE, uninitializedVault.rewardFee());
+        assertEq(admin, uninitializedVault.protocolFeeReceiver());
+        assertEq(admin, uninitializedVault.feeDistributor());
+
+        // Comet must have max allowance for the purposes of supplying WETH for cWETHv3.
+        assertEq(
+            type(uint256).max,
+            ERC20(USDC).allowance(address(vault), COMET)
+        );
+
+        assertEq(
+            type(uint256).max,
+            ERC20(COMP).allowance(address(vault), ROUTER)
+        );
+    }
+}
--- test/Helper.sol
+++ test/Helper.sol
@@ -8,29 +8,31 @@ import {ICometRewards} from "src/interfaces/ICometRewards.sol";
 import {BrrUSDC} from "src/BrrUSDC.sol";
 
 contract Helper is Test {
-    ERC1967Factory public constant _ERC1967_FACTORY =
+    ERC1967Factory public constant ERC1967_FACTORY =
         ERC1967Factory(0x0000000000006396FF2a80c067f99B3d2Ab4Df24);
-    address public constant _COMET_REWARDS =
+    address public constant COMET = 0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf;
+    address public constant COMET_REWARDS =
         0x123964802e6ABabBE1Bc9547D72Ef1B69B00A6b1;
-    address public constant _ROUTER =
-        0xafaE5a94e6F1C79D40F5460c47589BAD5c123B9c;
-    uint256 public constant _INITIAL_REWARD_FEE = 1_000;
-    address public immutable _admin = address(this);
-    address public immutable _vaultImplementation = address(new BrrUSDC());
+    address public constant ROUTER = 0xafaE5a94e6F1C79D40F5460c47589BAD5c123B9c;
+    uint256 public constant INITIAL_REWARD_FEE = 1_000;
+    address public constant USDC = 0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA;
+    address internal constant COMP = 0x9e1028F5F1D5eDE59748FFceE5532509976840E0;
+    address public immutable admin = address(this);
+    address public immutable vaultImplementation = address(new BrrUSDC());
     BrrUSDC public immutable vault;
 
     constructor() {
         vault = BrrUSDC(
-            _ERC1967_FACTORY.deployAndCall(
-                _vaultImplementation,
-                _admin,
+            ERC1967_FACTORY.deployAndCall(
+                vaultImplementation,
+                admin,
                 abi.encodeWithSelector(
                     BrrUSDC.initialize.selector,
-                    _COMET_REWARDS,
-                    _ROUTER,
-                    _INITIAL_REWARD_FEE,
-                    _admin,
-                    _admin
+                    COMET_REWARDS,
+                    ROUTER,
+                    INITIAL_REWARD_FEE,
+                    admin,
+                    admin
                 )
             )
         );
