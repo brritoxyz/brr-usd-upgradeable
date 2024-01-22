3bd4044c00fe2e451f264a029408f16cd9e15525
kphed <kpcrypto69@gmail.com>
2024-01-22 11:31:00 -0500

    Update default router

--- test/BrrUSD.t.sol
+++ test/BrrUSD.t.sol
@@ -1,13 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "forge-std/Test.sol";
 import {ERC20} from "solady/tokens/ERC20.sol";
 import {ERC4626} from "solady/tokens/ERC4626.sol";
 import {Initializable} from "solady/utils/Initializable.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 import {Helper} from "test/Helper.sol";
 import {BrrUSD} from "src/BrrUSD.sol";
 
 contract BrrUSDTest is Helper {
+    using SafeTransferLib for address;
+
+    function _getCUSDbC(uint256 amount) internal returns (uint256 balance) {
+
+        console.log("a", COMET.balanceOf(address(this)));
+
+        deal(COMET, address(this), amount);
+
+        balance = COMET.balanceOf(address(this));
+
+        console.log("b", COMET.balanceOf(address(this)));
+    }
+
     /*//////////////////////////////////////////////////////////////
                              initialize
     //////////////////////////////////////////////////////////////*/
@@ -176,4 +191,119 @@ contract BrrUSDTest is Helper {
         assertEq(shares, vault.balanceOf(to));
         assertLe(totalSupplyAfter, totalAssetsAfter);
     }
+
+    /*//////////////////////////////////////////////////////////////
+                             deposit
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotDepositInsufficientAssetBalance() external {
+        uint256 assets = type(uint256).max;
+        address to = address(this);
+
+        assertLt(COMET.balanceOf(address(this)), assets);
+
+        vm.expectRevert(BrrUSD.InsufficientAssetBalance.selector);
+
+        vault.deposit(assets, to);
+    }
+
+    function testCannotDepositInsufficientAssetBalanceFuzz(
+        uint256 assets
+    ) external {
+        vm.assume(assets != 0);
+
+        address to = address(this);
+
+        assertLt(COMET.balanceOf(address(this)), assets);
+
+        vm.expectRevert(BrrUSD.InsufficientAssetBalance.selector);
+
+        vault.deposit(assets, to);
+    }
+
+    // function testDeposit() external {
+    //     uint256 assets = _getCWETH(1e18);
+    //     address to = address(this);
+    //     uint256 totalSupplyBefore = vault.totalSupply();
+    //     uint256 totalAssetsBefore = vault.totalAssets();
+
+    //     // Comet rounds down transfer amounts, making it difficult to check the final emitted values.
+    //     vm.expectEmit(true, true, true, false, address(vault));
+
+    //     emit ERC4626.Deposit(address(this), to, assets, 0);
+
+    //     uint256 shares = vault.deposit(assets, to);
+    //     uint256 totalSupplyAfter = vault.totalSupply();
+    //     uint256 totalAssetsAfter = vault.totalAssets();
+    //     uint256 expectedShares = vault.convertToShares(
+    //         totalAssetsAfter - totalAssetsBefore,
+    //         totalSupplyBefore,
+    //         totalAssetsBefore
+    //     );
+
+    //     assertEq(expectedShares, shares);
+    //     assertEq(shares, totalSupplyAfter - totalSupplyBefore);
+    //     assertEq(shares, vault.balanceOf(to));
+    //     assertLe(totalSupplyAfter, totalAssetsAfter);
+    // }
+
+    // function testDepositMultiple() external {
+    //     uint256 baseAsset = 0.001 ether;
+    //     uint256 totalSupply = 0;
+    //     uint256 totalAssets = 0;
+
+    //     for (uint256 i = 0; i < anvilAccounts.length; ++i) {
+    //         uint256 asset = _getCWETH(baseAsset * (i + 1));
+    //         uint256 totalSupplyBefore = vault.totalSupply();
+    //         uint256 totalAssetsBefore = vault.totalAssets();
+
+    //         vm.expectEmit(true, true, true, false, address(vault));
+
+    //         emit ERC4626.Deposit(address(this), anvilAccounts[i], asset, 0);
+
+    //         uint256 shares = vault.deposit(asset, anvilAccounts[i]);
+    //         uint256 totalSupplyAfter = vault.totalSupply();
+    //         uint256 totalAssetsAfter = vault.totalAssets();
+    //         uint256 expectedShares = vault.convertToShares(
+    //             totalAssetsAfter - totalAssetsBefore,
+    //             totalSupplyBefore,
+    //             totalAssetsBefore
+    //         );
+    //         totalSupply += totalSupplyAfter - totalSupplyBefore;
+    //         totalAssets += totalAssetsAfter - totalAssetsBefore;
+
+    //         assertLt(0, shares);
+    //         assertEq(expectedShares, shares);
+    //         assertEq(shares, totalSupplyAfter - totalSupplyBefore);
+    //         assertEq(shares, vault.balanceOf(anvilAccounts[i]));
+    //         assertLe(totalSupplyAfter, totalAssetsAfter);
+    //     }
+
+    //     assertEq(totalSupply, vault.totalSupply());
+    //     assertEq(totalAssets, vault.totalAssets());
+    // }
+
+    // function testDepositFuzz(uint80 assets, address to) external {
+    //     assets = uint80(_getCWETH(assets));
+    //     uint256 totalSupplyBefore = vault.totalSupply();
+    //     uint256 totalAssetsBefore = vault.totalAssets();
+
+    //     vm.expectEmit(true, true, true, false, address(vault));
+
+    //     emit ERC4626.Deposit(address(this), to, assets, 0);
+
+    //     uint256 shares = vault.deposit(assets, to);
+    //     uint256 totalSupplyAfter = vault.totalSupply();
+    //     uint256 totalAssetsAfter = vault.totalAssets();
+    //     uint256 expectedShares = vault.convertToShares(
+    //         totalAssetsAfter - totalAssetsBefore,
+    //         totalSupplyBefore,
+    //         totalAssetsBefore
+    //     );
+
+    //     assertEq(expectedShares, shares);
+    //     assertEq(shares, totalSupplyAfter - totalSupplyBefore);
+    //     assertEq(shares, vault.balanceOf(to));
+    //     assertLe(totalSupplyAfter, totalAssetsAfter);
+    // }
 }
--- test/Helper.sol
+++ test/Helper.sol
@@ -14,7 +14,7 @@ contract Helper is Test {
     address public constant COMET = 0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf;
     address public constant COMET_REWARDS =
         0x123964802e6ABabBE1Bc9547D72Ef1B69B00A6b1;
-    address public constant ROUTER = 0xafaE5a94e6F1C79D40F5460c47589BAD5c123B9c;
+    address public constant ROUTER = 0xe88483B5901FA3537355C4324ccF92a8d4155260;
     uint256 public constant INITIAL_REWARD_FEE = 1_000;
     address public constant USDC = 0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA;
     address public constant COMP = 0x9e1028F5F1D5eDE59748FFceE5532509976840E0;
